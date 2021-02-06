9ce340af97f9361838f0e49f8edbb278f56276f8
khakhalin <khakhalin@gmail.com>
2021-02-06 17:39:46 -0500

    unit tests  moved to pytest

--- esn/utils.py
+++ esn/utils.py
@@ -2,25 +2,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
-def plot_data(x, y, title='Lorenz attractor'):
-    if not x.ndim == 1:
-       raise ValueError("Argument x_array should be 1 dimensional. "
-                         "It actually is {0} dimensional".format(x.ndim))
-    if not y.ndim == 1:
-        raise ValueError("Argument y_array should be 1 dimensional. "
-                         "It actually is {0} dimensional".format(y.ndim))
-    #if len(x) != len(y):
-    #    raise RuntimeError("Arguments x_array and y_array should have same length. "
-    #                       "But x_array has length {0} and y_array has length {1}".format(len(x),
-    #                                                                                      len(y)
-    #                                                                                      )
-    #                       )
-    fig, ax = plt.subplots()
-    ax.plot(x, y, ".")
-    ax.set(xlabel="X-comp", ylabel="Z-comp", title=title)
-    return fig
 # Static class, never initialized
-
 class utils():
     """A collection of relevant utilities."""
 
@@ -34,3 +16,16 @@ class utils():
         nx.draw_kamada_kawai(G, node_size=30)
         return G
 
+    @staticmethod
+    def plot_data(x, y, title='Lorenz attractor'):
+        """AKH NOTE: I have no idea why we would need this. But keeping for now."""
+        if not x.ndim == 1:
+            raise ValueError("Argument x_array should be 1 dimensional. "
+                             "It actually is {0} dimensional".format(x.ndim))
+        if not y.ndim == 1:
+            raise ValueError("Argument y_array should be 1 dimensional. "
+                             "It actually is {0} dimensional".format(y.ndim))
+        fig, ax = plt.subplots()
+        ax.plot(x, y, ".")
+        ax.set(xlabel="X-comp", ylabel="Z-comp", title=title)
+        return fig
---
+++ tests/__init__.py
@@ -0,0 +1,2 @@
+# Empty, needed for correct importing
+# https://stackoverflow.com/questions/25827160/importing-correctly-with-pytest
